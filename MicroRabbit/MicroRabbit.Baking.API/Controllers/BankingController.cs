0955402b28854e74de6e15025020426a19e8de22
Romero Medeiros <romeromeireles@hotmail.com>
2021-04-03 12:38:39 -0300

    Adds BankingController with basic Accounts listing

---
+++ MicroRabbit/MicroRabbit.Baking.API/Controllers/BankingController.cs
@@ -0,0 +1,23 @@
+﻿using MicroRabbit.Banking.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroRabbit.Baking.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BankingController : ControllerBase
+    {
+        private readonly IAccountAppService _accountAppService;
+
+        public BankingController(IAccountAppService accountAppService)
+        {
+            _accountAppService = accountAppService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_accountAppService.GetAccounts());
+        }
+    }
+}
