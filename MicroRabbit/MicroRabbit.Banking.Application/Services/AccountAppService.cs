d019c37b28b8edae305e1fa8e899b06e1ab6deba
Romero Medeiros <romeromeireles@hotmail.com>
2021-04-03 12:19:08 -0300

    Creates and configures Banking.Application, Banking.Data; Configures DI; Adjust ConfigureServices() in API; Adds first migration (Account model);

---
+++ MicroRabbit/MicroRabbit.Banking.Application/Services/AccountAppService.cs
@@ -0,0 +1,24 @@
+﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Banking.Application.Services
+{
+    public class AccountAppService : IAccountAppService
+    {
+        private readonly IAccountRepository _accountRepository;
+
+        public AccountAppService(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+
+        public IEnumerable<Account> GetAccounts()
+        {
+            return _accountRepository.GetAccounts();
+        }
+    }
+}
--- MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
+++ MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -1,9 +1,11 @@
-﻿using MicroRabbit.Domain.Core.Bus;
+﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Services;
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Banking.Data.Repository;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infra.Bus;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace MicroRabbit.Infra.IoC
 {
@@ -13,6 +15,13 @@ namespace MicroRabbit.Infra.IoC
         {
             // DomainBus
             services.AddTransient<IEventBus, RabbitMQBus>();
+
+            // Application Services
+            services.AddTransient<IAccountAppService, AccountAppService>();
+
+            // Data
+            services.AddTransient<IAccountRepository, AccountRepository>();
+            services.AddTransient<BankingDbContext>();
         }
     }
 }
